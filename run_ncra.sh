a5e6983c2e97212a51f9bd8f5d02c94d534add93
Jason Stajich <jasonstajich.phd@gmail.com>
2026-07-18 09:47:53 -0700

    rename

--- run_ncra.sh
+++
@@ -1,5 +0,0 @@
-module load nextflow
-
-nextflow run main.nf -resume --config configs/pezio5_fungi.csv --data_dir data --run_tool phmmer --pfam_hmm db/pfam/38.2/Pfam-A.hmm \
-	--swissprot_dmnd db/uniprot/uniprot_sprot.fasta.dmnd --modelorgs_config configs/modelorgs.yaml -profile slurm --project pezizo5
-
